4bcc14cebb92e298c3c9429db068a1dbea0211ff
racdale@gmail.com <racdale@gmail.com>
2018-06-15 10:33:04 -0700

    updating windowed_sindy

--- R/windowed_sindy.R
+++ R/windowed_sindy.R
@@ -10,7 +10,24 @@
 
 .packageName <- 'sindyr'
 
-windowed_sindy = function(xs,dx=NULL,dt=1,Theta=NULL,lambda=.05,
-  window.size=round(length(xs)/10),window.shift=round(length(xs)/10)) {
-
+windowed_sindy = function(xs,dx=NULL,dt=1,Theta=NULL,lambda=.05,fit.its=10,B.expected=NULL,
+                          window.size=round(nrow(xs)/10),window.shift=round(nrow(xs)/10)) {
+  j = 1
+  windowed.results = c()
+  for (i in seq(from=1,to=nrow(xs)-window.size+1,by=window.shift)) {
+    sub.xs = xs[i:(i+window.size-1),]
+    if (!is.null(dx)) {
+      sub.dx = dx[i:(i+window.size-1),]
+    } else {
+      sub.dx = NULL
+    }    
+    sindy.obj = sindy(sub.xs,dx=as.matrix(sub.dx),dt=dt,Theta=Theta,lambda=lambda,B.expected=B.expected,verbose=F,fit.its=fit.its,plot.eq.graph=F)
+    j = j + 1
+    windowed.results = rbind(windowed.results,data.frame(t=i,window=j,window.size=window.size,
+                                                         B.err=ifelse(is.null(sindy.obj$B.err),NA,sindy.obj$B.err),
+                                                         pred.err=sindy.obj$pred.err,
+                                                         simple.kolmog=sindy.obj$simple.kolmog,
+                                                         prop.coef=sindy.obj$prop.coef))
+  }
+  return(windowed.results)
 }
